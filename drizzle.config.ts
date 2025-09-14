8513cb2cc0c7f90e0d48276aa290f95bc548e1f2
cybregaz <kkanttechy@gmail.com>
2025-09-14 15:07:34 +0530

    user modeL

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "drizzle-kit";
 import "dotenv/config";
 
+if (!process.env.DB_URL) {
+  throw new Error("DB_URL is not set in .env");
+}
+
 export default defineConfig({
   dialect: "postgresql",
   schema: "./src/models/**/*.ts",
