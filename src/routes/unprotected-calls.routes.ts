a3548df5e5b2f740d376d58a7369d958a9aafbad
cybregaz <kkanttechy@gmail.com>
2025-09-30 22:35:20 +0530

    calling test x19

--- src/routes/unprotected-calls.routes.ts
+++ src/routes/unprotected-calls.routes.ts
@@ -7,13 +7,9 @@ import { eq } from "drizzle-orm";
 
 const unprotected_call_routes = new Elysia({ prefix: "/call" })
 
-  .post("/decline", async ({ body }) => {
-    const [call_info] = await db.select().from(call_model).where(eq(call_model.id, body.call_id)).limit(1);
-    await CallService.decline_call(body.call_id, call_info.callee_id);
-  }, {
-    body: t.Object({
-      call_id: t.Number()
-    })
+  .post("/decline/:call_id", async ({ params }) => {
+    const [call_info] = await db.select().from(call_model).where(eq(call_model.id, Number(params.call_id))).limit(1);
+    await CallService.decline_call(Number(params.call_id), call_info.callee_id);
   })
 
 export default unprotected_call_routes;
